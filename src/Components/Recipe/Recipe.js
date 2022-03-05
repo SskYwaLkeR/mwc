bc03db8db22264d412ea243538ed503efc246135
Hrishikesh <hkrai39@gmail.com>
2022-03-05 06:43:25 +0530

    add recipe

--- src/Components/Recipe/Recipe.js
+++ src/Components/Recipe/Recipe.js
@@ -3,8 +3,13 @@ import "./Recipe.css";
 
 export const Recipe = () => {
   return (
-    <div>
-      <h1>Recipe</h1>
+    <div className={"recipe-container"}>
+      <img className={"ingredients"} src="/recipe.png" alt="" />
+      <div className={"action"}>
+        <a className={"download"} href="/recipe.png" download>
+          Download Recipe
+        </a>
+      </div>
     </div>
   );
 };
